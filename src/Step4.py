e7c5e7fdc59d9541abf9f0a2aa9966d5a3d5a28a
KhyatiGanatra <ubuntu@ip-172-31-41-20.us-west-2.compute.internal>
2018-10-16 02:02:49 +0000

    adding code for infilling

--- src/Step4.py
+++ src/Step4.py
@@ -1,5 +1,9 @@
+# This script is for training the dataset
+
 import gc
 import datetime
+import keras
+import tensorflow as tf
 
 import pandas as pd
 import numpy as np
@@ -9,6 +13,10 @@ from tqdm import tqdm
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import TensorBoard
 from keras import backend as K
+
+config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 4} ) 
+sess = tf.Session(config=config) 
+keras.backend.set_session(sess)
 
 import cv2
 import matplotlib
@@ -24,36 +32,34 @@ import os
 import re
 
 def get_latest_weights_file():
-	log_dir = "/home/ubuntu/Insight_AI/PConv-Keras/data/logs/"
-	for r, d, files in os.walk(log_dir):
-		latest_file = max(files, key=os.path.getctime)
-	absname = log_dir + latest_file
-	return absname
-f = get_latest_weights_file()
-print (f)
+	log_dir = "/home/ubuntu/Insight_AI/PConv-Keras_KKG/data/logs/"
+	files = os.listdir(log_dir)
+	absnames = []
+	for file in files:
+		absnames.append(log_dir + file)
+	latest_file = max(absnames, key=os.path.getctime)
+	return latest_file
+#f = get_latest_weights_file()
+
 
 plt.ioff()
 
-# SETTINGS
-#TRAIN_DIR = r"/home/khyati/Documents/Insight/sports_images/train_1/"
-#TEST_DIR = r"/home/khyati/Documents/Insight/sports_images/test_1"
-#VAL_DIR = r"/home/khyati/Documents/Insight/sports_images/val_1/"
+TRAIN_DIR = r"/home/ubuntu/Insight_AI/building_images/train_1/"
+TEST_DIR = r"/home/ubuntu/Insight_AI/building_images/test_1/"
+VAL_DIR = r"/home/ubuntu/Insight_AI/building_images/val_1/"
 
-TRAIN_DIR = r"/home/ubuntu/Insight_AI/sports_images/train_1/"
-TEST_DIR = r"/home/ubuntu/Insight_AI/sports_images/test_1/"
-VAL_DIR = r"/home/ubuntu/Insight_AI/sports_images/val_1/"
-
-BATCH_SIZE = 4
-
+BATCH_SIZE = 5
 
 class DataGenerator(ImageDataGenerator):
 	def flow_from_directory(self, directory, *args, **kwargs):
 		generator = super().flow_from_directory(directory, class_mode=None, *args, **kwargs)
+
 		while True:
 			# Get augmentend image samples
 			ori = next(generator)
-
 			# Get masks for each image sample
+			#fileName = FILE_DIR + 'resized_COCA_COLA.jpg'
+			#print (fileName)
 			mask = np.stack([random_mask(ori.shape[1], ori.shape[2]) for _ in range(ori.shape[0])], axis=0)
 
 			# Apply masks to all image sample
@@ -90,6 +96,7 @@ test_generator = test_datagen.flow_from_directory(
 	TEST_DIR, target_size=(512, 512), batch_size=BATCH_SIZE, seed=1
 )
 
+#Pick out an example
 test_data = next(test_generator)
 (masked, mask), ori = test_data
 
@@ -99,7 +106,7 @@ for i in range(len(ori)):
 	axes[0].imshow(masked[i,:,:,:])
 	axes[1].imshow(mask[i,:,:,:] * 1.)
 	axes[2].imshow(ori[i,:,:,:])
-	plt.show()
+	#plt.show()
 
 
 def plot_callback(model):
@@ -120,20 +127,22 @@ def plot_callback(model):
 		axes[1].set_title('Predicted Image')
 		axes[2].set_title('Original Image')
 
-		plt.savefig(r'data/test_samples/img_{}_{}.png'.format(i, pred_time))
+		plt.savefig(r'data/custom_test_samples/img_{}_{}.png'.format(i, pred_time))
 		plt.close()
 
 # Instantiate the model
+model = PConvUnet()
 model = PConvUnet(weight_filepath='data/logs/')
 latest_weights = get_latest_weights_file()
+print (latest_weights)
 model.load(latest_weights)
 
 # Run training for certain amount of epochs
 model.fit(
 	train_generator,
-	steps_per_epoch=50,
+	steps_per_epoch=100,
 	validation_data=val_generator,
-	validation_steps=5,
+	validation_steps=10,
 	epochs=50,
 	plot_callback=plot_callback,
 	callbacks=[
@@ -153,9 +162,9 @@ model.load(
 # Run training for certain amount of epochs
 model.fit(
 	train_generator, 
-	steps_per_epoch=50,
+	steps_per_epoch=100,
 	validation_data=val_generator,
-	validation_steps=5,
+	validation_steps=10,
 	epochs=50,	
 	workers=3,
 	plot_callback=plot_callback,
@@ -175,6 +184,7 @@ model.load(
 
 n = 0
 for (masked, mask), ori in tqdm(test_generator):
+	print(masked, mask, "-----")	
 
 	# Run predictions for this batch of images
 	pred_img = model.predict([masked, mask])
@@ -192,11 +202,11 @@ for (masked, mask), ori in tqdm(test_generator):
 		axes[1].xaxis.set_major_formatter(NullFormatter())
 		axes[1].yaxis.set_major_formatter(NullFormatter())
 
-		plt.savefig(r'data/test_samples/img_{}_{}.png'.format(i, pred_time))
+		plt.savefig(r'data/custom_test_samples/img_{}_{}.png'.format(i, pred_time))
 		plt.close()
 		n += 1
 
 	# Only create predictions for about 100 images
-	if n > 100:
+	if n > 10:
 		break
 
